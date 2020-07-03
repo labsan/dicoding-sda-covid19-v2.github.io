6f8f3d811d8067c7cdbcd2987d3b1c979a808901
labsan <59597107+labsan@users.noreply.github.com>
2020-07-03 21:54:36 +0700

    Delete navbar.js
    file navbar.js tidak lagi digunakan

--- assets/js/navbar.js
+++
@@ -1,8 +0,0 @@
-function toggle() {
-    var x = document.getElementById('id-toggle');
-    if (x.className === 'class-toggle') {
-        x.className += ' responsive';
-    } else {
-        x.className = 'class-toggle';
-    }
-}
